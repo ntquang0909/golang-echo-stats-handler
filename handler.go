265b66024846516a6afbe4d95e6992318576e667
Thái Tấn Lợi <thaitanloi365@gmail.com>
2020-07-15 01:45:59 +0700

    export function

--- handler.go
+++ handler.go
@@ -16,7 +16,7 @@ var nsInMs float64 = float64(time.Millisecond)
 
 var statsMux sync.Mutex
 
-func statsHandler(c echo.Context) error {
+func Handler(c echo.Context) error {
 	statsMux.Lock()
 	defer statsMux.Unlock()
 
